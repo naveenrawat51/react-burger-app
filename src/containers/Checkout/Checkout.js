a05f1032ccf1a5f74f71a7ecb62900cd4fb73fef
Naveen Rawat <naveen.rawat@sprint.com>
2020-06-11 17:39:43 +0530

    linting added

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,7 +12,7 @@ class Checkout extends Component {
     componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
-        let price = 0;
+        const price = 0;
         for (let param of query.entries()) {
             if(param[0] === 'price'){
                 price = param[1];
