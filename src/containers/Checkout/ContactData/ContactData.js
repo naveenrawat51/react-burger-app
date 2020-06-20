03557fbe13b318c9ed2b8d28ce95545daf46bb82
Naveen Rawat <naveen.rawat@sprint.com>
2020-06-20 12:38:24 +0530

    test

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -160,7 +160,7 @@ class ContactData extends Component {
                 config: this.state.orderForm[key]
             })
         }
-        let form = (
+        const form = (
             <form onSubmit={this.orderHandler}>
                 {formElementsArray.map((formElement, index) => (
                     <Input
